9c6df6ff8a6ccbc6656b6b2aa6c350509d47788c
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-05 12:27:05 +0530

    fetching current user with useAuthHook and creating new hook to store in firestore database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
+import useAuthUser from "./hooks/useAuthUser";
 import useWindowSize from "./hooks/useWindowSize";
 
 export default function App() {
 	const page = useWindowSize();
+	const user = useAuthUser();
+	if (!user) {
+		return <Login />;
+	}
 
 	return (
 		<div className="app" style={{ ...page }}>
-			<Login />
+			App
 		</div>
 	);
 }
