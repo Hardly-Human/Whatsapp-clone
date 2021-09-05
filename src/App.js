3dae1ea15d9e582f3aec71c40bbbb09fbd704c39
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-05 11:59:30 +0530

    Added Login with Google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
 import useWindowSize from "./hooks/useWindowSize";
 
 export default function App() {
-  const page = useWindowSize();
+	const page = useWindowSize();
 
-  return (
-    <div className="app" style={{ ...page }}>
-      App
-    </div>
-  );
+	return (
+		<div className="app" style={{ ...page }}>
+			<Login />
+		</div>
+	);
 }
