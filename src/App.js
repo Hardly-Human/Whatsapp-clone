071df5d2775a571770ef2d12ba8131b9b6ee2b58
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-05 12:50:48 +0530

    Added Sidebar Component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 import Login from "./components/Login";
 import useAuthUser from "./hooks/useAuthUser";
 import useWindowSize from "./hooks/useWindowSize";
+import Sidebar from "./components/Sidebar.js";
 
 export default function App() {
 	const page = useWindowSize();
 	const user = useAuthUser();
+
 	if (!user) {
 		return <Login />;
 	}
 
 	return (
 		<div className="app" style={{ ...page }}>
-			App
+			<div className="app__body">
+				<Sidebar user={user} page={page} />
+			</div>
 		</div>
 	);
 }
