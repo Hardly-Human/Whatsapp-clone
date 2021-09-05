fe11d3e29caf7c773e2bd5b2e109e87d05641f75
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-05 18:08:00 +0530

    Added chat component

--- src/hooks/useRoom.js
+++ src/hooks/useRoom.js
@@ -1,1 +1,22 @@
-export default function useRoom() {}
+import { useDocument } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
+export default function useRoom(roomId, userId) {
+	const isUserRoom = roomId.includes(userId);
+
+	const doc = isUserRoom ? roomId?.replace(userId, "") : roomId;
+
+	const [snapshot] = useDocument(
+		db.collection(isUserRoom ? "rooms" : "users").doc(doc)
+	);
+
+	if (!snapshot) return null;
+
+	return {
+		id: snapshot.id,
+		photoURL:
+			snapshot.photoURL ||
+			`https://avatars.dicebear.com/api/human/${snapshot.id}.svg`,
+		...snapshot.data(),
+	};
+}
