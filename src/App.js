e57c9411737c30f5b7b4e9d1ea93c37d033e14bb
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-05 13:07:36 +0530

    Added Routing to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Chat from "./components/Chat";
 import useAuthUser from "./hooks/useAuthUser";
 import useWindowSize from "./hooks/useWindowSize";
 import Sidebar from "./components/Sidebar.js";
+import { Route, Redirect } from "react-router-dom";
 
 export default function App() {
 	const page = useWindowSize();
@@ -15,8 +17,12 @@ export default function App() {
 
 	return (
 		<div className="app" style={{ ...page }}>
+			<Redirect to={page.isMobile ? "/chats" : "/"} />
 			<div className="app__body">
 				<Sidebar user={user} page={page} />
+				<Route path="/room/:roomId">
+					<Chat user={user} page={page} />
+				</Route>
 			</div>
 		</div>
 	);
