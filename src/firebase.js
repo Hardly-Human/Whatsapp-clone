3c37faaea31bc7558395120a204e8d7082b6efd6
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-11-18 09:10:49 +0530

    Updated Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
-import 'firebase/database';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/database";
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyDTT6qrw4aMYu6fj_cLH3YwQIkuH4_7NVg',
-	authDomain: 'project-whatsapp-clone-58cb1.firebaseapp.com',
-	projectId: 'project-whatsapp-clone-58cb1',
-	storageBucket: 'project-whatsapp-clone-58cb1.appspot.com',
-	messagingSenderId: '206550990037',
-	appId: '1:206550990037:web:9ef18a7642787764dff65c',
+	apiKey: "AIzaSyC09rBy9aYiJsBt67zG_JCcEpMAMEgOGEw",
+	authDomain: "whatsapp-clone-e7410.firebaseapp.com",
+	projectId: "whatsapp-clone-e7410",
+	storageBucket: "whatsapp-clone-e7410.appspot.com",
+	messagingSenderId: "491015387626",
+	appId: "1:491015387626:web:56b9f443506e11c98d1a93",
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -18,8 +18,8 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage().ref('images');
-const audioStorage = firebase.storage().ref('audios');
+const storage = firebase.storage().ref("images");
+const audioStorage = firebase.storage().ref("audios");
 const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
 const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
 
